b01f23b2390d016383853edc8fb9278ca5d5e2ea
Toan Dao Minh <toandaominh1997@gmail.com>
2018-04-11 03:30:40 +0700

    kaka
    df

---
+++ app/src/main/java/com/example/dell/bluetooth/Const.java
@@ -0,0 +1,6 @@
+package com.example.dell.bluetooth;
+
+public class Const {
+    static final String TAG = "SPP_TERMINAL";
+}
+
---
+++ app/src/main/java/com/example/dell/bluetooth/Utils.java
@@ -0,0 +1,118 @@
+package com.example.dell.bluetooth;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Log;
+
+
+public class Utils {
+    public static void log(String message) {
+        if (BuildConfig.DEBUG) {
+            if (message != null) Log.i(Const.TAG, message);
+        }
+    }
+    public static String printHex(String hex) {
+        StringBuilder sb = new StringBuilder();
+        int len = hex.length();
+        try {
+            for (int i = 0; i < len; i += 2) {
+                sb.append("0x").append(hex.substring(i, i + 2)).append(" ");
+            }
+        } catch (NumberFormatException e) {
+            log("printHex NumberFormatException: " + e.getMessage());
+
+        } catch (StringIndexOutOfBoundsException e) {
+            log("printHex StringIndexOutOfBoundsException: " + e.getMessage());
+        }
+        return sb.toString();
+    }
+    public static byte[] toHex(String hex) {
+        int len = hex.length();
+        byte[] result = new byte[len];
+        try {
+            int index = 0;
+            for (int i = 0; i < len; i += 2) {
+                result[index] = (byte) Integer.parseInt(hex.substring(i, i + 2), 16);
+                index++;
+            }
+        } catch (NumberFormatException e) {
+            log("toHex NumberFormatException: " + e.getMessage());
+
+        } catch (StringIndexOutOfBoundsException e) {
+            log("toHex StringIndexOutOfBoundsException: " + e.getMessage());
+        }
+        return result;
+    }
+    public static byte[] concat(byte[] A, byte[] B) {
+        byte[] C = new byte[A.length + B.length];
+        System.arraycopy(A, 0, C, 0, A.length);
+        System.arraycopy(B, 0, C, A.length, B.length);
+        return C;
+    }
+    public static int mod(int x, int y) {
+        int result = x % y;
+        return result < 0 ? result + y : result;
+    }
+    public static String calcModulo256(String command)
+    {
+        int crc = 0;
+        for (int i = 0; i< command.length(); i++) {
+            crc += (int)command.charAt(i);
+        }
+        return Integer.toHexString(Utils.mod(crc, 256));
+    }
+    // ============================================================================
+
+    /**
+     * Раскрасить текст нужным цветом
+     */
+    public static String mark(String text, String color) {
+        return "<font color=" + color + ">" + text + "</font>";
+    }
+    // ============================================================================
+
+    /**
+     * Получение id сохранённого в игрушке звукового набора
+     */
+    public static String getPrefence(Context context, String item) {
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        return settings.getString(item, Const.TAG);
+    }
+    // ============================================================================
+
+
+    /**
+     * Получение флага из настроек
+     */
+    public static boolean getBooleanPrefence(Context context, String tag) {
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        return settings.getBoolean(tag, true);
+    }
+    // ============================================================================
+
+
+    /**
+     * Класс-фильтр полей ввода
+     */
+    // ============================================================================
+    public static class InputFilterHex implements InputFilter {
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            for (int i = start; i < end; i++) {
+                if (!Character.isDigit(source.charAt(i))
+                        && source.charAt(i) != 'A' && source.charAt(i) != 'D'
+                        && source.charAt(i) != 'B' && source.charAt(i) != 'E'
+                        && source.charAt(i) != 'C' && source.charAt(i) != 'F'
+                        ) {
+                    return "";
+                }
+            }
+            return null;
+        }
+    }
+    // ============================================================================
+}
